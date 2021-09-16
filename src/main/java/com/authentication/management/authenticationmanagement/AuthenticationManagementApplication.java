b9d1c22866b7a3004dfb7569c120002b0d88d3bd
Sachin <sachinkmr375@gmail.com>
2021-09-16 22:40:46 +0530

    first commit

---
+++ src/main/java/com/authentication/management/authenticationmanagement/AuthenticationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.authentication.management.authenticationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authentication/management/authenticationmanagement/AuthenticationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentication.management.authenticationmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
