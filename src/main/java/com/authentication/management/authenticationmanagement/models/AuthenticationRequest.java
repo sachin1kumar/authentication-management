97a58bffd48e041ff77f2c6380e99b713484dc0a
Sachin <sachinkmr375@gmail.com>
2021-09-16 22:42:41 +0530

    Model classes added.

---
+++ src/main/java/com/authentication/management/authenticationmanagement/models/AuthenticationRequest.java
@@ -0,0 +1,20 @@
+package com.authentication.management.authenticationmanagement.models;
+
+public class AuthenticationRequest {
+
+    private String username;
+    private String password;
+
+    public AuthenticationRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
---
+++ src/main/java/com/authentication/management/authenticationmanagement/models/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.authentication.management.authenticationmanagement.models;
+
+public class AuthenticationResponse {
+
+    private String jwtToken;
+
+    public AuthenticationResponse(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+
+    public String getJwtToken() {
+        return jwtToken;
+    }
+}
